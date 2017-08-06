2bb1e24025c1c9ec2c3c91a403ae2d21a53885bf
Shiung <shiungchenyu@gmail.com>
2017-08-06 16:19:26 +0800

    loading 微調

--- loading.php
+++ loading.php
@@ -15,7 +15,7 @@
 		</div>
 		<div class="loadScroll">
 			<ul>
-				<li>在Matches，距離從來都不是問題 !</li>
+				<li>在Matches，距離從來都不是問題 !!!</li>
 				<li>你知道每天有多少人在Matches相遇嗎 ?</li>
 				<li>只要你相信，Matches每天都賦予你奇蹟 !</li>
 				<li>聽說每個人只有六萬之一的機率可以遇到同一個人...</li>
